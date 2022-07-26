6d6bb7f1c303a496290933d3479962cfae3c8922
asmhasansarker <asmhasansarker@gmail.com>
2022-07-26 12:23:36 +0600

    url updated

--- config/config.js
+++ config/config.js
@@ -5,7 +5,7 @@ const dev = {
     port: process.env.PORT || 4000,
   },
   db: {
-    url: process.env.DB_URL || "mongodb://localhost:27017/TestNytti",
+    // url: process.env.DB_URL || "mongodb://localhost:27017/TestNytti",
     url: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bnvlz.mongodb.net/${process.env.DB_NAME}`,
   },
 };
